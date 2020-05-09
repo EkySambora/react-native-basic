beb30f470f17d2c1647c3be82678f4b764945ce5
Eky Zulianto Arif <ekysambora@gmail.com>
2020-05-09 19:23:07 +0700

    hide scroll indicator

--- components/propsDinamis/index.js
+++ components/propsDinamis/index.js
@@ -20,23 +20,54 @@ const Story = (props) => {
             }}>{props.title}</Text>
         </View>
     )
+};
+
+const MiddleSection = () => {
+    return(
+        <View>
+            <Text style={{color:'#fff',fontSize:20,textAlign:"center", marginBottom:30}}>Halo middle section</Text>
+
+            <View style={{marginBottom:20}}>
+                <Image source={{uri: 'https://picsum.photos/200/300'}} style={{width:'100%', height:100}} />
+            </View>
+        </View>
+    )
 }
 
 const PropsDinamis = () => {
     return(
-        <View>
-            <Text style={{textAlign:"center", fontSize:20, textDecorationLine:"underline"}}>Belajar React Native</Text>
-            <ScrollView horizontal>
-                <View style={{flexDirection:'row'}}>
-                    <Story title="Judul Satu" img="https://picsum.photos/200/300" />
-                    <Story title="Judul Dua" img="https://picsum.photos/200/300" />
-                    <Story title="Judul Tiga" img="https://picsum.photos/200/300" />
-                    <Story title="Judul Empat" img="https://picsum.photos/200/300" />
-                    <Story title="Judul Lima" img="https://picsum.photos/200/300" />
+        <ScrollView  showsVerticalScrollIndicator={false}>
+            <View>
+                <Text style={{textAlign:"center", fontSize:20, fontWeight:"bold"}}>React Native App</Text>
+                <ScrollView horizontal  showsHorizontalScrollIndicator={false}>
+                    <View style={{flexDirection:'row'}}>
+                        <Story title="Judul Satu" img="https://picsum.photos/200/300" />
+                        <Story title="Judul Dua" img="https://picsum.photos/200/300" />
+                        <Story title="Judul Tiga" img="https://picsum.photos/200/300" />
+                        <Story title="Judul Empat" img="https://picsum.photos/200/300" />
+                        <Story title="Judul Lima" img="https://picsum.photos/200/300" />
+                        <Story title="Judul Enam" img="https://picsum.photos/200/300" />
+                        <Story title="Judul Tujuh" img="https://picsum.photos/200/300" />
+                        <Story title="Judul Delapan" img="https://picsum.photos/200/300" />
+                        <Story title="Judul Sembilan" img="https://picsum.photos/200/300" />
+                    </View>
+                </ScrollView>
+            
+                <View style={{
+                    backgroundColor:"#123",
+                    marginTop:20,
+                    padding:30
+                }}>
+                    <MiddleSection/>
+                    <MiddleSection/>
+                    <MiddleSection/>
+                    <MiddleSection/>
+                    <MiddleSection/>
+                    <MiddleSection/>
                 </View>
-            </ScrollView>
 
-        </View>
+            </View>
+        </ScrollView>
     )
 }
 
